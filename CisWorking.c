294aa6f98ab9dc0c47a7bcb01ad7cc2b4fc4df50
dcambur <dcambur@gmail.com>
2019-10-30 17:35:22 +0200

    Tan/Cot + Cos fix

--- CisWorking.c
+++ CisWorking.c
@@ -2,7 +2,7 @@
 
 double sinD(double x) {
     int i, n=10;
-    float sum,t;
+    float sum=1,t=1;
     x= x*(3.14159/180.0);
     t=x;
     sum=x;
@@ -15,22 +15,30 @@ double sinD(double x) {
 }
 
 double cosD(double x) {
-    int i, n=20;
-    float sum,t;
-    x= x*(3.14159/180.0);
-    t=x;
-    sum=x;
+    int i, n=10;
+    float sum=1,t=1;
+    x= x*3.14159/180.0;
 
     for (i=1;i<=n;i++) {
-        t=t*(-1)*x*x/(2*i*(2*i-1));
+        t=(t*(-1)*x*x)/(2*i*(2*i-1));
         sum+=t;
     }
     return sum;
 }
+
+double tanD(double x) {
+    return sinD(x)/cosD(x);
+}
+
+double cotD(double x) {
+    return cosD(x)/sinD(x);
+}
+
+
 int main() {
     double x;
     printf("Enter x:");
     scanf("%lf",&x);
-    printf("cos:%lf, sin:%lf",cosD(x), sinD(x));
+    printf("cos:%lf, sin:%lf, tan:%lf, cot:%lf",cosD(x), sinD(x), tanD(x),cotD(x));
     return 0;
 }
