f777f4aa90bd17f7b1aef9e29a22beb215b478bc
Nick <NickBasso@users.noreply.github.com>
2019-10-30 14:13:58 +0200

    first commit

---
+++ Source Files/Solution.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <cstring>
+#include "coreMath.h"
+
+const int N = (int)(1e5);
+
+
+
+int main(){
+	// input block
+	freopen("input.txt", "r", stdin);	
+	char s[N] = {0};
+	gets(s);
+	printf("%s\n", s);
+	
+	
+	
+	return 0;
+}
