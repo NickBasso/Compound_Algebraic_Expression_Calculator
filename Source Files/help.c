a52c7d2f0c7563b5f31fee9c2872035a66b406f0
Nick <NickBasso@users.noreply.github.com>
2019-11-06 14:13:41 +0200

    Update help.c

--- Source Files/help.c
+++ Source Files/help.c
@@ -1,46 +1,24 @@
 #include <stdio.h>
+#include "..\Headers\PredefinedDesignations.h"
 
-double StringToDouble(char *s){
-	/*132
-	1 * 0 + 2
-	1 * 10 + 3
-	1 * 100 + 1*/
-	
-	int i = 0;
-	int dots = 0;
-	
-	while(s[i] >= '0' && s[i] <= '9' || s[i] == ' '){
-		if(s[i] == '.'){
-			dots++;
-		}
-			
-		i++;
+double pow(double number, int exponent){
+	if(exponent == 0){
+		return 1.0;
 	}
 	
-	int length = i;
+	double base = number;
+	bool isNegative = false;
 	
-	if(dots > 1){
-		printf("WRONG\n");
-		return 0;
+	if(exponent < 0){
+		isNegative = true;
 	}
 	
-	double number = 0.0;
-	double curDigit;
+	for(int i = 1; i < exponent; i++){
+		number *= base;
+	}
 	
-	int k = 0;
-	int j;
-	
-	for(i = length - 1; i >= 0; i--){
-		curDigit = (s[i] - '0') % 10;
-		
-		int powerOfTen = 1;
-		
-		for(j = 0; j < k; j++)
-			powerOfTen *= 10;
-			
-		number += powerOfTen * curDigit;
-		
-		k++;
+	if(isNegative == true){
+		number = 1.0 / number;
 	}
 	
 	return number;
@@ -48,10 +26,10 @@ double StringToDouble(char *s){
 
 main(){
 	//printf("%lf", sin(10000));
+
+	double a = 3, double b = 3;
 	
-	char s[10] = "213--";
-	
-	printf("%lf", StringToDouble(s));
+	printf("%lf", pow(a, b));
 	
 	return 0;
 }
