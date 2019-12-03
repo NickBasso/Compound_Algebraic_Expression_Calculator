39e40cc3988948ca1474a0ab0c69528adebc9f3e
Nick <NickBasso@users.noreply.github.com>
2019-12-03 16:33:33 +0200

    trash removed

--- Source Files/Solution.c
+++ Source Files/Solution.c
@@ -1,13 +1,8 @@
-/*
-TO DO:
-	exponential functions
-*/
-
 #include <stdio.h>
 
-// !Warning! DON'T CHANGE THE ORDER! SOB@KI
+// !Warning! DON'T CHANGE THE ORDER!
 #include "..\Headers\Exceptions.h"					// Exceptions
-#include "..\Headers\PredefinedDesignations.h"		// Predefined Designations (Предопределенные обозначения #define)
+#include "..\Headers\PredefinedDesignations.h"		// Predefined Designations (пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ #define)
 #include "..\Headers\MathConstants.h"				// Mathematical constants
 #include "..\Headers\CoreMath.h"					// Algebraic functions
 #include "..\Headers\Strings.h"						// Operations on strings
@@ -16,26 +11,25 @@ TO DO:
 int main(){
 	// input block
 	freopen("input.txt", "r", stdin);
-		
+
 	char s[N] = {0};
 	gets(s);
 	printf("%s\n", s);
-	
+
 	// set operation priority for the main algorithm logic
 	operationPriority['+'] = operationPriority['-'] = -2;
 	operationPriority['*'] = operationPriority['/'] = operationPriority['^'] = -1;
-	
-	// remove all spaces and print the string again 
+
+	// remove all spaces and print the string again
 	if(RemoveSpaces(s) == true){
 		printf("\nNormalized form of the expression:\n%s\n\n", s);
 	}
-	
+
 	// calculate the expression value
 	double ans = calculate(s);
-	
+
 	// print the answer with the 15-digit precision
 	printf("%.15lf", ans);
-	
+
 	return 0;
 }
-
